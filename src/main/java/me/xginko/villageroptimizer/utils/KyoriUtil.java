acdb97685d3e5d36bcebcfaf3812044125fbad0c
xGinko <rachel.audrey@mail.de>
2024-02-21 14:35:56 +0100

    remove unused method

--- src/main/java/me/xginko/villageroptimizer/utils/KyoriUtil.java
+++ src/main/java/me/xginko/villageroptimizer/utils/KyoriUtil.java
@@ -1,12 +1,8 @@
 package me.xginko.villageroptimizer.utils;
 
 import me.xginko.villageroptimizer.VillagerOptimizer;
-import net.kyori.adventure.identity.Identity;
 import net.kyori.adventure.text.Component;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
-import java.util.Locale;
 
 public class KyoriUtil {
 
@@ -16,9 +12,5 @@ public class KyoriUtil {
 
     public static void sendActionBar(CommandSender sender, Component message) {
         VillagerOptimizer.getAudiences().sender(sender).sendActionBar(message);
-    }
-
-    public static Locale getLocale(Player player) {
-        return VillagerOptimizer.getAudiences().player(player).pointers().getOrDefault(Identity.LOCALE, Locale.US);
     }
 }
